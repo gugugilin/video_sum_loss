cafa9354e842e745ea1f2423a98c27e4af7a6c1e
gugugi_lin <lily09290110@gmail.com>
2017-02-22 18:21:18 +0800

    input_test.txt

---
+++ ROUGE_L.py
@@ -0,0 +1,76 @@
+from nltk.corpus import wordnet as wn
+import math
+
+class ROUGE_L:
+    def __init__(self):
+        return 
+    def set_inputs(self,s1):
+        self.inputs=s1.split(" ")
+    def set_targes(self,lis):
+        self.targes=[]
+        lis_len=len(lis)
+        for i in range(lis_len):
+            self.targes.append(lis[i].split(" "))
+    def myactive_function(self,cost):
+        x=math.pi*(2*cost-1)
+        outloss=1/(1+math.exp(-1*x))
+        return outloss
+
+    def mycross_entropy(self,cost):
+        outloss=-1*math.log(self.myactive_function(cost))
+        return outloss
+
+    def similar_score(self,s1,s2):
+        w1=wn.synsets(s1)
+        w2=wn.synsets(s2)
+        if len(w1)==0 or len(w2)==0:
+            if s1==s2:
+                return 0.99
+            else:
+                return 0
+        out=w1[0].path_similarity(w2[0])
+        return out
+
+    def LCS(self,index_targes):
+        s1=self.inputs
+        s2=self.targes[index_targes]
+        baseline=2.0/math.pi
+        m=len(s1)+1
+        n=len(s2)+1
+        record=[[0 for i in range(n)] for j in range(m)]
+        for i in range(1,m):
+            for j in range(1,n):
+                word_score=self.similar_score(s1[i-1],s2[j-1])
+                if word_score!=None and word_score>=baseline:
+                    record[i][j] = record[i-1][j-1]+word_score
+                elif record[i-1][j] >= record[i][j-1]:
+                    record[i][j] = record[i-1][j]
+                else:
+                    record[i][j] = record[i][j-1]
+        return record[m-2][n-1]
+
+    def Caculate_max_LCS(self):
+        if len(self.targes)<=0:
+            return -1
+        if len(self.inputs)<=0:
+            return -1
+        targes_len=len(self.targes)
+        max_p=0
+        max_q=0
+        index_targes=0
+        for i in range(targes_len):
+            if len(self.targes[i])>0:
+                LCS_score=self.LCS(i)
+                if max_q<LCS_score:
+                    max_q=LCS_score
+                if max_p<(LCS_score/len(self.targes)):
+                    max_p=LCS_score/len(self.targes)
+        return max_p,(max_q/len(self.inputs))
+    def Caculate_ROUGE_L(self):
+        max_p,max_q=self.Caculate_max_LCS()
+        if max_p==0 or max_q==0:
+            return 100000
+        alpha=1.2
+        F=(1+alpha**2)*max_p*max_q/(max_p+(alpha**2)*max_q)
+        return self.mycross_entropy(F)
+
---
+++ test_ROUGE.py
@@ -0,0 +1,23 @@
+import ROUGE_L as rouge
+import time
+
+start = time.time()
+inputs = [line.rstrip('\n') for line in open("input_test.txt")]
+target_content = [line.rstrip('\n') for line in open("target_test.txt")]
+targes=[]
+for i in target_content:
+    temp=[]
+    temp.append(i)
+    targes.append(temp)
+r1=rouge.ROUGE_L()
+end = time.time()
+print("load time:",end-start)
+
+for i in range(len(inputs)):
+    print("begin exec")
+    r1.set_inputs(inputs[i])
+    r1.set_targes(targes[i])
+    start = time.time()
+    a=r1.Caculate_ROUGE_L()
+    end = time.time()
+    print("time: ",end-start,"score: ",a)
